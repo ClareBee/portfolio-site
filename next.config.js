5ba9c8bcb085c2b4a0e6152dbf9a83a424ab512e
Clare <chblackburne@gmail.com>
2019-12-23 14:46:05 +0000

    use withPlugins

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
-const withSass = require('@zeit/next-sass');
-module.exports = withSass({
-  cssModules: false
+const withPlugins = require('next-compose-plugins');
+
+const sass = require('@zeit/next-sass');
+
+const bundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
 });
+
+const withTM = require('next-transpile-modules');
+
+// module.exports = withTM({
+//   transpileModules: ['somemodule', 'and-another']
+// });
+
+module.exports = withPlugins([sass], [bundleAnalyzer]);

d4cd0fe165921d372ae2246f9fc6b5e145e70869
Clare <chblackburne@gmail.com>
2019-12-23 19:33:41 +0000

    add basic social media icons

---
+++ components/Icons.js
@@ -0,0 +1,11 @@
+import { FaTwitter, FaLinkedinIn, FaGithub } from "react-icons/fa";
+
+const Icons = () => (
+  <div>
+    <a><FaTwitter /></a>
+    <a><FaLinkedinIn /></a>
+    <a><FaGithub /></a>
+  </div>
+);
+
+export default Icons;
--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,11 @@
 import Layout from '../components/Layout';
+import Icons from '../components/Icons';
 
 export default function Contact() {
   return (
     <Layout title="Contact">
       <p>Contact page</p>
+      <Icons />
     </Layout>
   );
 }

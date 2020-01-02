ad0bdd413c7b4772e5f0bdbdb2a1b1a0b2bffc02
Clare <chblackburne@gmail.com>
2020-01-02 10:24:55 +0000

    index and show for blogs

---
+++ components/BlogRoll.js
@@ -0,0 +1,42 @@
+import Link from 'next/link';
+import ReactMarkdown from 'react-markdown';
+
+const BlogRoll = (props) => {
+  console.log('blog roll', props)
+  function truncateSummary(content) {
+    return content.slice(0, 200).trimEnd();
+  }
+
+  function reformatDate(fullDate) {
+    const date = new Date(fullDate)
+    return date.toDateString().slice(4);
+  }
+
+  return (
+    <>
+      <ul>
+        {props.allBlogs.length >= 1 && props.allBlogs.map(blog => (
+          <Link
+            key={blog.slug}
+            href={{ pathname: `/blog/${blog.slug}` }}
+          >
+            <a>
+            <li>
+              <div>
+                <img src={blog.document.data.banner} alt={blog.document.data.banner} />
+              </div>
+              <div>
+                <h2>{blog.document.data.title}</h2>
+                <h3> {reformatDate(blog.document.data.date)}</h3>
+                <ReactMarkdown source={truncateSummary(blog.document.content)} />
+              </div>
+            </li>
+            </a>
+          </Link>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default BlogRoll;
---
+++ pages/blog/[slug].js
@@ -0,0 +1,30 @@
+import matter from 'gray-matter'
+import ReactMarkdown from 'react-markdown'
+import Layout from '../../components/Layout'
+
+export default function Blog(props) {
+  const frontmatter = props.data
+  const markdownContent = props.content
+  return (
+    <Layout title={props.siteTitle}>
+      <article>
+        <h1>{frontmatter.title}</h1>
+        <div>
+          <ReactMarkdown source={markdownContent} />
+        </div>
+      </article>
+    </Layout>
+  )
+}
+
+Blog.getInitialProps = async function(context) {
+  const { slug } = context.query
+  const content = await import(`../../blog_posts/${slug}.md`)
+  const config = await import(`../../data/config.json`)
+  // gray-matter parses yaml frontmatter from md file
+  const data = matter(content.default)
+  return {
+    siteTitle: config.title,
+    ...data,
+  }
+}

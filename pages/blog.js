4e8597e6c16cea72c67b1897402f5e980ef64cc6
Clare <chblackburne@gmail.com>
2020-01-02 10:25:12 +0000

    blog page

--- pages/blog.js
+++ pages/blog.js
@@ -1,14 +1,48 @@
+import matter from 'gray-matter';
 import Layout from '../components/Layout';
-import Link from 'next/link';
+import Icons from '../components/Icons';
+import BlogRoll from '../components/BlogRoll';
 
-export default function Blog() {
 
+const Blog = (props) => {
+  console.log('props', props)
   return (
     <Layout title="Blog">
-      <p>Blog page</p>
-      <Link href="/blog/[pid]" as="/blog/first-blogpost">
-        <a>First Post</a>
-      </Link>
+      <p>Blog Page </p>
+      <BlogRoll allBlogs={props.allBlogs}/>
+      <Icons />
     </Layout>
   );
+}
+
+export default Blog;
+
+Blog.getInitialProps = async function() {
+  const siteConfig = await import(`../data/config.json`)
+   //get posts & context from folder
+   const blogPosts = (context => {
+    const keys = context.keys();
+    const values = keys.map(context);
+    const data = keys.map((key, index) => {
+      // Create slug from filename
+      const slug = key
+        .replace(/^.*[\\\/]/, "")
+        .split(".")
+        .slice(0, -1)
+        .join(".");
+      const value = values[index];
+      // gray-matter parses yaml metadata & markdownbody
+      const document = matter(value.default);
+      return {
+        document,
+        slug
+      };
+    });
+    return data;
+  })(require.context("../blog_posts", true, /\.md$/));
+
+  return {
+    allBlogs: blogPosts,
+    ...siteConfig,
+  }
 }

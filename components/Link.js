f1aa1f4006ce8fe1a0ae39d66182824b72119d3a
Clare <chblackburne@gmail.com>
2020-01-12 18:07:46 +0000

    nested nav link styling

--- components/Link.js
+++ components/Link.js
@@ -1,10 +1,13 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
+const nestedPath = (href, router) =>
+  href.match('blog') && router.pathname.match('blog')
+
 export default ({ href, children }) => {
   const router = useRouter();
   let className = children.props.className || '';
-  if (router.pathname === href && children.type !== 'svg') {
+  if ((router.pathname === href && children.type !== 'svg') || nestedPath(href, router)) {
     className = `${className} selected`
   }
   return <Link href={href}>{React.cloneElement(children, { className })}</Link>

7195105151d3b1cf93ce25036ea33edbbd9d7464
Clare <chblackburne@gmail.com>
2020-01-17 19:20:18 +0000

    make contact form style generic

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -67,36 +67,47 @@ export default () => {
   }
   return (
     <section className="contact-section">
-      <form onSubmit={handleOnSubmit} className="contact-form">
+      <form onSubmit={handleOnSubmit} className="form">
+      <div className="form__form-control">
         <label htmlFor="email">Your Email</label>
         <input
           id="email"
           type="email"
           name="_replyto"
           onChange={handleOnChange}
+          placeholder="you@example.com"
           required
           value={inputs.email}
         />
+        </div>
+        <div className="form__form-control">
         <label htmlFor="message">Your Message</label>
         <textarea
           id="message"
           name="message"
           onChange={handleOnChange}
+          placeholder="What's on your mind?"
           required
+          rows="5"
           value={inputs.message}
         />
-        <button type="submit" disabled={status.submitting}>
+        </div>
+        <div className="form__form-control">
+        <button type="submit" disabled={status.submitting} className="btn btn--primary">
           {!status.submitting
             ? !status.submitted
               ? 'Submit'
               : 'Submitted'
             : 'Submitting...'}
         </button>
+        </div>
+        {status.info.error && (
+          <div className="error">Error: {status.info.msg}</div>
+        )}
+        {!status.info.error && status.info.msg && <div className="msg">{status.info.msg}</div>}
+
       </form>
-      {status.info.error && (
-        <div className="error">Error: {status.info.msg}</div>
-      )}
-      {!status.info.error && status.info.msg && <p>{status.info.msg}</p>}
+
     </section>
   )
 }

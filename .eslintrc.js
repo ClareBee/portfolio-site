726eded0623a598bfd3e6d134ddc14dcae7bfc06
Clare <chblackburne@gmail.com>
2020-02-18 12:14:01 +0000

    react plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: [
+    'plugin:react/recommended'
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',

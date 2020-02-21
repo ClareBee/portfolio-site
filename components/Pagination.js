5962de6aba758c3e942a66110f07d8e0fbdb3146
Clare <chblackburne@gmail.com>
2020-02-21 15:55:47 +0000

    only request needed blogs and order by recency

--- components/Pagination.js
+++ components/Pagination.js
@@ -1,9 +1,19 @@
 import PropTypes from 'prop-types';
+import Link from './Link';
 
 const Pagination = props => {
-  console.log(props);
-
-  return <div>Pagination</div>;
+  const pageLinks = () => {
+    return props.pagesArray.map((page, index) => {
+      const style =
+        index + 1 === Number(props.currentPage) ? 'active' : '';
+      return (
+        <Link key={index} href={`/blog?page=${index + 1}`}>
+          <a className={style}>{index + 1}</a>
+        </Link>
+      );
+    });
+  };
+  return <div className="pagination">{pageLinks()}</div>;
 };
 
 export default Pagination;
--- pages/blog.js
+++ pages/blog.js
@@ -1,33 +1,20 @@
 import matter from 'gray-matter';
 import PropTypes from 'prop-types';
-import Link from '../components/Link';
-
 import Layout from '../components/Layout';
 import PageTitle from '../components/PageTitle';
 import PromotedBlog from '../components/PromotedBlog';
 import Pagination from '../components/Pagination';
 import BlogRoll from '../components/BlogRoll';
 
-const totalPerPage = 2;
+const PAGE_TOTAL = 2;
 
-const Blog = ({ allBlogs, currentPage }) => {
-  // TODO: handle this in initial Props
-  const totalBlogs = allBlogs.length;
-  const pages = Math.ceil(totalBlogs / totalPerPage);
-  const pagesArray = Array.from(Array(pages));
-
-  const promotedBlog = allBlogs.find(
-    blog => blog.document.data.promoted,
-  );
-
-  function orderByRecentFirst(blogs) {
-    // TODO: check this works
-    return blogs.sort(blog => blog.document.data.date).reverse();
-  }
-  // TODO: move into the initial request to only retrieve/parse those needed
-  const pagedBlogs = currentPage => {
-    return allBlogs.filter(blog => blog.page == currentPage);
-  };
+const Blog = ({
+  allBlogs,
+  currentPage,
+  pagesArray,
+  promotedBlog,
+  total,
+}) => {
   return (
     <Layout title="Blog">
       <PageTitle
@@ -36,13 +23,11 @@ const Blog = ({ allBlogs, currentPage }) => {
       />
       <div className="blog-layout">
         {promotedBlog && <PromotedBlog blog={promotedBlog} />}
-        <BlogRoll allBlogs={pagedBlogs(currentPage)} />
-        <Pagination />
-        {pagesArray.map((page, index) => (
-          <Link key={index} href={`/blog?page=${index + 1}`}>
-            <a>{index + 1}</a>
-          </Link>
-        ))}
+        <BlogRoll allBlogs={allBlogs} />
+        <Pagination
+          pagesArray={pagesArray}
+          currentPage={currentPage}
+        />
       </div>
     </Layout>
   );
@@ -56,18 +41,30 @@ Blog.propTypes = {
 export default Blog;
 
 Blog.getInitialProps = async function(context) {
-  const totalPerPage = 2;
   const siteConfig = await import(`../data/config.json`);
   const query = context.query.page ? context.query.page : 1;
   // get total posts to display
-  const posts = query * totalPerPage;
-  const startingIndex = posts - totalPerPage;
-  const endingIndex = posts;
+  const startingIndex = query * PAGE_TOTAL - PAGE_TOTAL;
+  const endingIndex = startingIndex + PAGE_TOTAL;
+  let total;
+
   //get posts & context from folder
   const blogPosts = (context => {
     const keys = context.keys();
-    const values = keys.map(context);
-    const data = keys.map((key, index) => {
+    total = keys.length;
+    const needed = context
+      .keys()
+      .sort(
+        (a, b) =>
+          new Date(Array.from(b).splice(0, 9)) -
+          new Date(Array.from(a).splice(0, 9)),
+      )
+      .reverse()
+      .slice(startingIndex, endingIndex);
+    console.log(startingIndex, endingIndex);
+    console.log('needed', needed);
+    const values = needed.map(context);
+    const data = needed.map((key, index) => {
       // Create slug from filename
       const slug = key
         .replace(/^.*[\\/]/, '')
@@ -77,19 +74,29 @@ Blog.getInitialProps = async function(context) {
       const value = values[index];
       // gray-matter parses yaml metadata & markdownbody
       const document = matter(value.default);
-      // TODO: sort by recent last
       return {
         document,
         slug,
-        page: Math.ceil(totalPerPage / (index + 1)),
+        page: Math.ceil(PAGE_TOTAL / (index + 1)),
       };
     });
     return data;
   })(require.context('../blog_posts', true, /\.md$/));
 
+  const promotedBlog = blogPosts.find(
+    blog => blog.document.data.promoted,
+  );
+
+  const pagesArray = Array.from(
+    Array(Number(Math.ceil(total / PAGE_TOTAL))),
+  );
   return {
     allBlogs: blogPosts,
+    // pagedBlogs: pagedBlogs(query),
+    pagesArray,
+    total: total,
     currentPage: query,
+    promotedBlog,
     ...siteConfig,
   };
 };

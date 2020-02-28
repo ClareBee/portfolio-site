392a5eca1a04913355eb135b190973cb723fc84c
Clare <chblackburne@gmail.com>
2020-02-28 15:39:08 +0000

    wip gsap animations

--- pages/about.js
+++ pages/about.js
@@ -1,35 +1,87 @@
+import { useEffect } from 'react';
 import Layout from '../components/Layout';
+import PageTitle from '../components/PageTitle';
+
+// TODO: lazy load this
 import { gsap } from 'gsap/dist/gsap';
 import { MotionPathPlugin } from 'gsap/dist/MotionPathPlugin.js';
 //without this line, PixiPlugin and MotionPathPlugin may get dropped by your bundler (tree shaking)...
 
 export default function About() {
   gsap.registerPlugin(MotionPathPlugin);
+  useEffect(() => {
+    gsap.registerPlugin(MotionPathPlugin);
+    gsap.set('.blah, .hello', { transformOrigin: '50% 50%' });
+    gsap.to('.blah, .hello', {
+      duration: 2,
+      rotation: 360,
+      onUpdate: function() {
+        console.log('updated');
+      },
+      onStart: function() {
+        console.log('started');
+      },
+      onComplete: function() {
+        console.log('ended');
+      },
+    });
+    gsap.to('.blah', {
+      duration: 3,
+      x: 300,
+      backgroundColor: '#fff',
+      border: '5px solid white',
+      borderRadius: '5px',
+      // TODO: check gsap ease visualizer
+      ease: 'back',
+    });
+    gsap.from('.hello', {
+      duration: 1.5,
+      opacity: 0,
+      scale: 0.3,
+      y: 'random(-200, 200)',
+      stagger: 0.25,
+    });
+    gsap.set('.poop', {
+      width: 50,
+      height: 50,
+      xPercent: -50,
+      yPercent: -50,
+      transformOrigin: '50% 50%',
+    });
+    gsap.to('.poop', {
+      duration: 5,
+      motionPath: {
+        path: '#path', // can also pass it array of coordinates and it will smooth it out into a curve
+        autoRotate: true,
+        start: 0.25,
+        end: 1.5,
+        align: '#path',
+      },
+    });
+  }, []);
+
   return (
     <Layout title="About">
-      <p>About page</p>
-      <svg
-        id="text-container"
-        width="129"
-        height="115"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          id="text-curve"
-          d="M24.284 15.589C52.923 13.636 97.5 23 45.5 54c-52 31 50.043 17.88 72.784 26.589"
-          stroke="#000"
-          fill="none"
-        />
-        <text y="40">
-          <textPath
-            id="text-path"
-            startOffset="200"
-            href="#text-curve"
-          >
-            ClareBlackburne is really trying this out
-          </textPath>
-        </text>
-      </svg>
+      <PageTitle title="About" subtitle="" />
+      <div className="about-section">
+        <p className="blah">blah</p>
+        <div className="poop">poop</div>
+        <div className="hello">HELLLOOO+++++++</div>
+        <div className="hello">HELLLOOO+++++++</div>
+        <div className="hello">HELLLOOO+++++++</div>
+        <svg
+          width="190"
+          height="160"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            id="path"
+            d="M 10 80 Q 52.5 10, 95 80 T 180 80"
+            stroke="black"
+            fill="transparent"
+          />
+        </svg>
+      </div>
     </Layout>
   );
 }

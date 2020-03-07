df92f5eaa6458d77de0a9d9e84443f678f43538e
Clare <chblackburne@gmail.com>
2020-03-07 21:51:35 +0000

    wip animation

--- components/gsapwrapper.js
+++ components/gsapwrapper.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 import { gsap } from 'gsap/dist/gsap';
 import { MotionPathPlugin } from 'gsap/dist/MotionPathPlugin.js';
@@ -10,7 +10,6 @@ gsap.registerPlugin(MotionPathPlugin);
 gsap.registerPlugin(Draggable);
 
 const GsapWrapper = ({ children }) => {
-  const [point, setPoint] = useState(null);
   useEffect(() => {
     gsap.registerPlugin(MotionPathPlugin);
     // gsap.set('.blah, .hello', { transformOrigin: '50% 50%' });
@@ -49,24 +48,21 @@ const GsapWrapper = ({ children }) => {
     const myPath = document.getElementById('path');
     const myRawPath = MotionPathPlugin.getRawPath(myPath);
     console.log(myRawPath);
-    // let pointy = MotionPathPlugin.getPositionOnPath(
-    //   rawPath,
-    //   0.3,
-    //   false,
-    // );
-    // console.log('pointy', pointy);
 
     const ellipsesPoints = Array.from(ellipses)
       .sort((a, b) => {
         return a.id.split('-')[1] - b.id.split('-')[1];
       })
       .map(ell => {
-        return [ell.cx.baseVal.value, ell.cy.baseVal.value];
+        console.log('ell', ell);
+        return ell;
       });
     console.log(ellipsesPoints);
 
     const progressPoints = myLittlePath => {
       const steps = [
+        0,
+        0.02,
         0.05,
         0.1,
         0.15,
@@ -93,7 +89,7 @@ const GsapWrapper = ({ children }) => {
           position: MotionPathPlugin.getPositionOnPath(
             myLittlePath,
             step,
-            true,
+            false,
           ),
         };
       });
@@ -113,102 +109,119 @@ const GsapWrapper = ({ children }) => {
       );
     };
 
-    Draggable.create('#dragNavigator', {
-      type: 'x',
-      bounds: document.getElementById('container'),
-      inertia: true,
-      onClick: function() {
-        console.log('clicked');
-      },
-      onDragStart: function() {
-        console.log('drag started');
-        // clear styles on ellipses
-      },
-      onDragEnd: function() {
-        let i = circles.length;
-        while (--i > -1) {
-          if (this.hitTest(circles[i], overlapThreshold)) {
-            const progressNumber = circles[i].id.split('-')[1];
-            Array.from(ellipses).forEach(ellipse => {
-              if (ellipse.id.split('-')[1] === progressNumber) {
-                gsap.set(ellipse, {
-                  fill: 'green',
-                  scale: 3,
-                });
-                console.log('ellipse element', ellipse);
-                console.log(
-                  'ellipse',
-                  ellipse.getBoundingClientRect(),
-                );
-                const path = MotionPathPlugin.getRawPath('#path');
-                const length = MotionPathPlugin.getLength(path);
-                // console.log('length', length);
-                // const slice = MotionPathPlugin.sliceRawPath(
-                //   path,
-                //   startPoint,
-                //   ellipse,
-                // );
-                const client = ellipse.getBoundingClientRect();
-                const parent = document
-                  .getElementById('timelineSVG')
-                  .getBoundingClientRect();
-                console.log('parent', parent);
-                // const relativePos = {};
-                // relativePos.x = parent.x + client.x;
-                // relativePos.y = parent.y + client.y;
-
-                // console.log(relativePos);
-                const ellipseY = ellipse.getAttribute('cy');
-                const ellipseX = ellipse.getAttribute('cx');
-                console.log('xy', ellipseX, ellipseY);
-                const steps = progressPoints(path);
-                let progress;
-                steps.forEach(step => {
-                  if (
-                    withinRange(step.position.x, ellipseX, 25) &&
-                    withinRange(step.position.y, ellipseY, 25)
-                  ) {
-                    console.log('matching', step);
-                    progress = step.percent;
-                  }
-                });
-                steps.forEach(step => {
-                  console.log(step.position.x, step.position.y);
-                  console.log(ellipseX, ellipseY);
-                });
-                console.log('progress', progress);
-                // console.log(
-                //   'hi',
-                //   MotionPathPlugin.getRawPath('#path'),
-                // );
-                // let pointy = MotionPathPlugin.getPositionOnPath(
-                //   path,
-                //   0.3,
-                //   false,
-                // );
-                gsap.set('#navigator', {
-                  width: 20,
-                  height: 20,
-                  xPercent: -50,
-                  yPercent: -50,
-                  transformOrigin: '50% 50%',
-                });
-                gsap.to('#navigator', {
-                  duration: 20,
-                  motionPath: {
-                    path: '#path',
-                    autoRotate: true,
-                    start: 0,
-                    end: progress,
-                    align: '#path',
-                  },
-                });
-              }
+    const time = (duration, proportion) => {
+      return duration * proportion;
+    };
+    const timeline = gsap.timeline();
+    const dragEvent = () => {
+      Draggable.create('#dragNavigator', {
+        type: 'x',
+        bounds: document.getElementById('container'),
+        inertia: true,
+        onClick: function() {
+          console.log('clicked');
+        },
+        onDragStart: function() {
+          console.log('drag started');
+          Array.from(ellipses).forEach(ellipse => {
+            gsap.set(ellipse, {
+              scale: 1,
+              fill: '#da2497',
             });
+          });
+        },
+        onDragEnd: function() {
+          console.log('dragended');
+          let i = circles.length;
+          while (--i > -1) {
+            if (this.hitTest(circles[i], overlapThreshold)) {
+              const progressNumber = circles[i].id.split('-')[1];
+              Array.from(ellipses).forEach(ellipse => {
+                if (ellipse.id.split('-')[1] === progressNumber) {
+                  gsap.set(ellipse, {
+                    fill: 'green',
+                    scale: 3,
+                  });
+                  console.log('ellipse element', ellipse);
+                  console.log(
+                    'ellipse',
+                    ellipse.getBoundingClientRect(),
+                  );
+                  const path = MotionPathPlugin.getRawPath('#path');
+                  const length = MotionPathPlugin.getLength(path);
+                  const client = ellipse.getBoundingClientRect();
+                  const parent = document
+                    .getElementById('timelineSVG')
+                    .getBoundingClientRect();
+                  console.log('parent', parent);
+
+                  const ellipseY = ellipse.getAttribute('cy');
+                  const ellipseX = ellipse.getAttribute('cx');
+                  console.log('xy', ellipseX, ellipseY);
+                  const steps = progressPoints(path);
+                  let progress;
+                  steps.forEach(step => {
+                    if (
+                      withinRange(step.position.x, ellipseX, 25) &&
+                      withinRange(step.position.y, ellipseY, 25)
+                    ) {
+                      console.log('matching', step);
+                      progress = step.percent;
+                    }
+                  });
+                  if (progress === undefined) {
+                    console.log('why is it undefined?');
+                    return;
+                  }
+                  console.log('progress', progress);
+                  const lengthOfAnimation = time(20, progress);
+                  gsap.set('#navigator', {
+                    width: 20,
+                    height: 20,
+                    xPercent: -50,
+                    yPercent: -50,
+                    transformOrigin: '50% 50%',
+                  });
+                  const precedingPoints = Array.from(ellipses);
+                  const tl = gsap.timeline({
+                    repeat: 2,
+                    repeatDelay: 1,
+                  });
+                  tl.to([...precedingPoints], {
+                    duration: 1.5,
+                    scale: 0.97,
+                    ease: 'back',
+                    stagger: 0.3,
+                  });
+
+                  gsap.to('#navigator', {
+                    duration: lengthOfAnimation,
+                    motionPath: {
+                      path: '#path',
+                      autoRotate: true,
+                      start: 0,
+                      end: progress,
+                      align: '#path',
+                    },
+                    onComplete: function() {
+                      console.log('animation ended');
+                      timeline.reverse();
+                      gsap.set('#dragNavigator', {
+                        clearProps: 'all',
+                      });
+                      // gsap.set('#navigator', {
+                      //   clearProps: 'all',
+                      // });
+                    },
+                  });
+                }
+              });
+            }
           }
-        }
-      },
-    });
+        },
+      });
+    };
+    dragEvent();
   }, []);
 
   // then we can control the whole thing easily...
